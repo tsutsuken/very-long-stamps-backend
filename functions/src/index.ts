c1661810acd48cc689bc32c39d4353ee93a2d269
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-06-22 17:12:24 +0900

    feat: Fetch data of asset

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -26,6 +26,21 @@ export const fetchOpensea = functions.https.onRequest((request, response) => {
       const html = await page.content();
       functions.logger.info("html", html, { structuredData: true });
       uploadHTMLToStorage(html);
+
+      const assetAnchors = await page.$$(".Asset--anchor");
+      for (const assetAnchor of assetAnchors) {
+        const assetPageUrl = (await (
+          await assetAnchor.getProperty("href")
+        ).jsonValue()) as string;
+        functions.logger.info("assetPageUrl", assetPageUrl, {
+          structuredData: true,
+        });
+
+        const _assetInfo = assetInfo(assetPageUrl);
+        functions.logger.info("_assetInfo", _assetInfo, {
+          structuredData: true,
+        });
+      }
       //   const buffer = await page.screenshot({ fullPage: true });
       //   uploadImageToStorage(buffer);
       await browser.close();
@@ -33,6 +48,29 @@ export const fetchOpensea = functions.https.onRequest((request, response) => {
 
   response.send("fetchOpensea");
 });
+
+interface AssetInfo {
+  assetId: string;
+  contractAddress: string;
+  chain: string;
+}
+
+const assetInfo = (assetPageUrl: string): AssetInfo | null => {
+  // assetPageUrl example: https://opensea.io/assets/matic/0xc52d9642260830055c986a97794b7b27393edf5e/16
+  const elements = assetPageUrl.split("/");
+  if (elements.length <= 3) {
+    return null;
+  }
+
+  const lastIndex = elements.length - 1;
+  const assetInfo: AssetInfo = {
+    assetId: elements[lastIndex],
+    contractAddress: elements[lastIndex - 1],
+    chain: elements[lastIndex - 2],
+  };
+
+  return assetInfo;
+};
 
 const uploadHTMLToStorage = async (html: string) => {
   functions.logger.info("uploadHTMLToStorage", { structuredData: true });
@@ -48,19 +86,19 @@ const uploadHTMLToStorage = async (html: string) => {
   functions.logger.info("saved", { structuredData: true });
 };
 
-const uploadImageToStorage = async (buffer: string | Buffer) => {
-  functions.logger.info("uploadImageToStorage", { structuredData: true });
-  const bucket = admin.storage().bucket();
-  const fileName = Date.now().toString();
-  const savePath = "puppeteer_capture/" + fileName + ".jpeg";
-  const bucketFile = bucket.file(savePath);
-  await bucketFile.save(buffer, {
-    metadata: {
-      contentType: "image/png",
-    },
-  });
-  functions.logger.info("saved", { structuredData: true });
-};
+// const uploadImageToStorage = async (buffer: string | Buffer) => {
+//   functions.logger.info("uploadImageToStorage", { structuredData: true });
+//   const bucket = admin.storage().bucket();
+//   const fileName = Date.now().toString();
+//   const savePath = "puppeteer_capture/" + fileName + ".jpeg";
+//   const bucketFile = bucket.file(savePath);
+//   await bucketFile.save(buffer, {
+//     metadata: {
+//       contentType: "image/png",
+//     },
+//   });
+//   functions.logger.info("saved", { structuredData: true });
+// };
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
